3152eccef97a3b4b059ce383f474d6eb3bae16e3
Dem19 <Danya_melniK@mail.ru>
2021-11-25 01:12:13 +0500

    pre_docker_commit

--- app/front/run_front_server.py
+++ app/front/run_front_server.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, redirect, url_for, request, flash
-import json
+from flask import Flask, render_template, redirect, url_for, request, flash, get_flashed_messages
+import flask
 import pandas as pd
+import urllib.request
+from urllib.error import URLError
+import json
+
 
 app = Flask(__name__)
 app.config.update(
@@ -8,46 +12,83 @@ app.config.update(
     SECRET_KEY='fhfgfahhkmhjrtyrsthfgvcnvb',
 )
 
+
+def get_prediction(model, brand, year, transmission, mileage, fuel_type, tax, mpg, engineSize):
+    body = {
+        "brand": brand,
+        "model": model,
+        "year": year,
+        "transmission": transmission,
+        "mileage": mileage,
+        "fuelType": fuel_type,
+        "tax": tax,
+        "mpg": mpg,
+        "engineSize": engineSize,
+    }
+
+    myurl = "http://192.168.0.106:8180/predict"
+    req = urllib.request.Request(myurl)
+    req.add_header('Content-Type', 'application/json; charset=utf-8')
+    json_data = json.dumps(body)
+    json_data_as_bytes = json_data.encode('utf-8')
+    req.add_header('Content-Length', len(json_data_as_bytes))
+    response = urllib.request.urlopen(req, json_data_as_bytes)
+    return json.loads(response.read())['predictions']
+
+
 train_data = pd.read_csv('data/X_train.csv')
 
-# def get_prediction(description, company_profile, benefits):
-#     body = {'description': description,
-#                             'company_profile': company_profile,
-#                             'benefits': benefits}
-#
-#     myurl = "http://0.0.0.0:8180/predict"
-#     req = urllib.request.Request(myurl)
-#     req.add_header('Content-Type', 'application/json; charset=utf-8')
-#     jsondata = json.dumps(body)
-#     jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
-#     req.add_header('Content-Length', len(jsondataasbytes))
-#     #print (jsondataasbytes)
-#     response = urllib.request.urlopen(req, jsondataasbytes)
-#     return json.loads(response.read())['predictions']
+page_data = {
+    'menu': ['Главная страница', 'О сайте']
+}
 
-page_data = {'menu': ['Главная страница', 'О сайте']}
+allowed_data = {
+    'car_models': train_data.groupby('brand')['model'].apply(set),
+    'transmissions': train_data['transmission'].unique(),
+    'fuel_types': train_data['fuelType'].unique(),
+}
+
+
+def check_parameter(value, type_func, allowed_values: tuple, name):
+    try:
+        value = type_func(value)
+    except ValueError:
+        flash(f'{name} должен иметь тип {type_func.__name__}', category='error')
+    else:
+        if value < allowed_values[0] or value > allowed_values[1]:
+            flash(f'Введите значения {name} в диапазоне от {allowed_values[0]} до {allowed_values[1]}',
+                  category='error')
+
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    allowed_data = {
-        'brands': train_data['brand'].unique(),
-        'car_models': train_data.groupby('brand')['model'].apply(set),
-    }
     page_data.update(allowed_data)
     if request.method == 'POST':
-        print(1)
-        # data['description'] = request.form.get('description')
-        # data['company_profile'] = request.form.get('company_profile')
-        # data['benefits'] = request.form.get('benefits')
-        #
-        #
-        # try:
-        #     response = str(get_prediction(data['description'],
-        #                               data['company_profile'],
-        #                               data['benefits']))
-        #     print(response)
-        # except ConnectionError:
-        #     response = json.dumps({"error": "ConnectionError"})
-        # return redirect(url_for('predicted', response=response))
+        model, brand = flask.request.form.get('Модель').split(' - ')
+        year = flask.request.form.get('year')
+        transmission = flask.request.form.get('Коробка передач')
+        mileage = flask.request.form.get('mileage')
+        fuel_type = flask.request.form.get('Тип топлива')
+        tax = flask.request.form.get('tax')
+        mpg = flask.request.form.get('mpg')
+        engineSize = flask.request.form.get('engineSize')
+
+        check_parameter(year, int, (1997, 2020), 'Год')
+        check_parameter(mileage, int, (0, 260000), 'Пробег')
+        check_parameter(tax, int, (0, 600), 'Налог')
+        check_parameter(mpg, int, (2, 480), 'Расход')
+        check_parameter(engineSize, float, (0, 6.6), 'Обхем двигателя')
+
+        if not get_flashed_messages():
+            flash(f'Данные отправлены', category='success')
+            try:
+                response = get_prediction(model, brand, year, transmission, mileage, fuel_type, tax, mpg, engineSize)
+                print(response)
+            except ConnectionError:
+                response = json.dumps({"error": "ConnectionError"})
+            except URLError:
+                response = json.dumps({"error": "URLError"})
+            return redirect(url_for('predicted', response=response))
 
     return render_template('index.html', **page_data)
 
@@ -55,29 +96,7 @@ def index():
 @app.route('/predicted/<response>')
 def predicted(response):
     response = json.loads(response)
-    print(response)
     return render_template('predicted.html', response=response)
-
-
-@app.route('/predict_form', methods=['GET', 'POST'])
-def predict_form():
-    form = ClientDataForm()
-    data = dict()
-    if request.method == 'POST':
-        data['description'] = request.form.get('description')
-        data['company_profile'] = request.form.get('company_profile')
-        data['benefits'] = request.form.get('benefits')
-
-
-        try:
-            response = str(get_prediction(data['description'],
-                                      data['company_profile'],
-                                      data['benefits']))
-            print(response)
-        except ConnectionError:
-            response = json.dumps({"error": "ConnectionError"})
-        return redirect(url_for('predicted', response=response))
-    return render_template('form.html', form=form)
 
 
 if __name__ == '__main__':
--- app/run_server.py
+++ app/run_server.py
@@ -2,8 +2,6 @@ import dill
 import pandas as pd
 import os
 import flask
-from time import strftime
-import json
 
 dill._dill._reverse_typemap['ClassType'] = type
 
@@ -12,7 +10,7 @@ app = flask.Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def general():
-    return """Welcome to fraudelent prediction process. Please use 'http://<address>/predict' to POST"""
+    return """Welcome to auto price prediction process. Please use 'http://<address>/predict' to POST"""
 
 
 def load_model(model_path):
@@ -28,10 +26,8 @@ model = load_model(model_path)
 @app.route("/predict", methods=["POST"])
 def predict():
     data = {"success": False}
-    # dt = strftime("[%Y-%b-%d %H:%M:%S]")
     if flask.request.method == "POST":
         json_data = flask.request.get_json()
-        # logger.info(f'{dt} Data: description={description}, company_profile={company_profile}, benefits={benefits}')
 
         try:
             d = pd.DataFrame({"brand": [json_data['brand']],
